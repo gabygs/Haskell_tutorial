17413d9d2a4525a6efa5992d5e6cbbe3d9095fc2
Gaby Gongora <gabygongora@Lauras-iMac.local>
2014-03-11 05:04:22 -0600

    A polite helloworld in Haskell

---
+++ helloWorld.hs
@@ -0,0 +1,7 @@
+
+--main = putStrLn "Hello world, I am learning Haskell!"
+
+main = do
+	putStrLn "Hello, what's your name?"
+	name <- getLine
+	putStrLn ("Hi " ++ name ++ ", you are learning Haskell!")
