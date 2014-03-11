da83e590dd12e675221c0cfb68841d8a7a5e9d09
Gaby Gongora <gabygongora@Lauras-iMac.local>
2014-03-11 05:02:01 -0600

    Calculating a factorial

---
+++ factorial.hs
@@ -0,0 +1,8 @@
+factorial :: (Integral a) => a -> a 
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+factorial' :: (Integral a) => a -> a 
+factorial' n = if (n==0) 
+				then 1 
+				else n * factorial (n - 1)
